aef240a38ce9e1c7b7cedd40c75739ff06d53903
jeroh09 <tjdsagaciousman@gmail.com>
2025-08-19 14:09:41 +0100

    about section update

--- src/components/ProfileSettings/AboutSection.js
+++ src/components/ProfileSettings/AboutSection.js
@@ -8,6 +8,11 @@ const AboutSection = () => (
     <ul className="space-y-2">
       <li><Link to="/terms" className="text-blue-600 hover:underline">Terms of Service</Link></li>
       <li><Link to="/privacy" className="text-blue-600 hover:underline">Privacy Policy</Link></li>
+      <li><Link to="/community-guidelines" className="text-blue-600 hover:underline">Community Guidelines</Link></li>
+      <li><Link to="/copyright" className="text-blue-600 hover:underline">Copyright & IP Policy</Link></li>
+      <li><Link to="/security" className="text-blue-600 hover:underline">Security & Data Protection</Link></li>
+      <li><Link to="/ai-disclaimer" className="text-blue-600 hover:underline">AI Use Disclaimer</Link></li>
+      <li><Link to="/contractor-agreements" className="text-blue-600 hover:underline">Contractor Agreements</Link></li>
     </ul>
   </div>
 );
