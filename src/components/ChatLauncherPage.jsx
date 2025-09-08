1ecb66c755dcde5d1c41a1af4dcb33a19a6ab2eb
jeroh09 <39214152+jeroh09@users.noreply.github.com>
2025-09-08 03:17:35 +0100

    learn

--- src/components/ChatLauncherPage.jsx
+++ src/components/ChatLauncherPage.jsx
@@ -5,8 +5,8 @@ import { useUser } from '../contexts/UserContext';
 import { characterCategories } from '../data/characterCategories';
 import useInteractedCharacters from '../hooks/useInteractedCharacters';
 import usePremiumCharacters from '../hooks/usePremiumCharacters';
-import TemplateGallery from './components/TemplateGallery';
-import CharacterBuilder from './components/CharacterBuilder';
+import TemplateGallery from '../components/TemplateGallery';
+import CharacterBuilder from '../components/CharacterBuilder';
 import CharacterCreationSuccess from './components/CharacterCreationSuccess'; // âœ… NEW IMPORT
 import { usePremiumCharacterFlow } from '../hooks/usePremiumCharacterFlow';
 
