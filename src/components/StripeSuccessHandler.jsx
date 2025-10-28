a5bdbbc02da43e72c7b254a6dd5e375ce2fc1016
jeroh09 <tjdsagaciousman@gmail.com>
2025-10-28 10:49:03 +0000

    little

--- src/components/StripeSuccessHandler.jsx
+++ src/components/StripeSuccessHandler.jsx
@@ -65,7 +65,6 @@ export default function StripeSuccessHandler() {
   // ============================================================================
 
   useEffect(() => {
-    const { user } = useUser();
     if (!user) return;
 
 
