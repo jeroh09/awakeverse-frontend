f0b5581cb699495e46053fb448541c457c7aa46c
jeroh09 <tjdsagaciousman@gmail.com>
2025-10-31 23:05:17 +0000

    dfdfd

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
+import './utils/csrfInterceptor';  // ✅ Add this FIRST (before React imports)
 import { AuthProvider } from './contexts/AuthContext';
 import { UserProvider } from './contexts/UserContext';
 import { WebSocketProvider } from './contexts/WebSocketContext';
